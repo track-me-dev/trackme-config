ddd6ee7b1c1abf687fe5f48c18a534b173ebca6a
jinhyeon <vmflwu123@gmail.com>
2023-09-30 02:39:34 +0900

    initial commit

---
+++ src/test/java/com/app/trackmeconfig/TrackmeConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.trackmeconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmeConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
